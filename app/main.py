94bb050352c29784f4950b64794fd85023c97d48
scott-norm <scott@nomosai.com>
2024-01-26 09:53:36 -0500

    fixed several typos

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
-# main.py
 from fastapi import FastAPI, Query
 from app.utils import Output
 
 app = FastAPI()
 
 """
-Please create and endpoint that accepts a query string, e.g., "what happens if I steal 
+Please create an endpoint that accepts a query string, e.g., "what happens if I steal 
 from the Sept?" and returns a JSON response serialized from the Pydantic Output class.
 """
